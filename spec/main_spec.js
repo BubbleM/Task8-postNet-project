d255e11651d22cbb6510de70064592fc01af5fa3
BubbleM <1870408473@qq.com>
2017-08-10 23:18:32 +0800

    update

---
+++ spec/main_spec.js
@@ -0,0 +1,55 @@
+"use strict";
+var _ = require("lodash");
+var chai = require("chai");
+var sinon = require("sinon");
+var sinonChai = require("sinon-chai");
+var expect = chai.expect;
+chai.use(sinonChai);
+
+var main = require("../lib/main.js");
+
+
+describe("测试描述", function(){
+    sinon.spy(console, 'log');
+
+    it("return barCode while input zipCode length is 5", function(){
+
+        var result = main('95713');
+        var expect_string = '|　|:|::　:|:|:　|:::|　:::||　::||:　:|:|:　|';
+
+        expect(expect_string).to.equal(result);
+    });
+
+    it("return barCode while input zipCode length is 9", function(){
+
+        var result = main('450561234');
+        var expect_string = '|　:|::|　:|:|:　||:::　:|:|:　:||::　:::||　::|:|　::||:　:|::|　||:::　|';
+
+        expect(expect_string).to.equal(result);
+    });
+
+    it("return barCode while input zipCode length is 10", function(){
+
+        var result = main('45056-1234');
+        var expect_string = '|　:|::|　:|:|:　||:::　:|:|:　:||::　:::||　::|:|　::||:　:|::|　||:::　|';
+
+        expect(expect_string).to.equal(result);
+    });
+
+    it('return zipCode while input barCode length is 5', function () {
+      let str = '|　|:|::　:|:|:　|:::|　:::||　::||:　:|:|:　|';
+      var result = main(str);
+        var expect_string = '95713';
+
+        expect(expect_string).to.equal(result);
+    });
+
+    it('return zipCode while input barCode length is 10', function () {
+        let str = '|　:|::|　:|:|:　||:::　:|:|:　:||::　:::||　::|:|　::||:　:|::|　||:::　|';
+        var result = main(str);
+        var expect_string = '45056-1234';
+
+        expect(expect_string).to.equal(result);
+    });
+
+});
